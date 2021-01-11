53b0841fc9cb7bc0f6c9be36f0df19e0a918a904
vlad ageysha <ander123fakk@gmail.com>
2021-01-11 12:50:17 +0300

    made a function to display the current window

--- src/App/App.js
+++ src/App/App.js
@@ -5,14 +5,25 @@ import NoteList from './components/NoteList/index';
 import './style.css';
 
 const App = () => {
+    function switchWindow(e) {
+        const btn = e.target;
+        const tabList = btn.parentElement.parentElement;
+
+        for (let i = 0; i < tabList.children.length; i++) {
+            tabList.children[i].firstChild.classList.remove('active-tab');
+        }
+
+        btn.classList.add('active-tab');
+    }
+
     return(
         <div className="app">
             <HashRouter>
 
                 <ul className="tab-list">
-                    <NavLink to="/create"><li className="tab active-tab">Создать</li></NavLink>
-                    <NavLink to="/notes"><li className="tab">Заметки</li></NavLink>
-                    <NavLink to="/done-notes"><li className="tab">Готовые</li></NavLink>
+                    <NavLink to="/create"><li className="tab active-tab" onClick={(e) => switchWindow(e)}>Создать</li></NavLink>
+                    <NavLink to="/notes"><li className="tab" onClick={(e) => switchWindow(e)}>Заметки</li></NavLink>
+                    <NavLink to="/done-notes"><li className="tab" onClick={(e) => switchWindow(e)}>Готовые</li></NavLink>
                 </ul>
                 <div className="content">
                     <Route path="/create" component={Form} />
