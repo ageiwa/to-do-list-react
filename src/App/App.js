a9b46b52f45d8a3e9a88ba644204d277678b1811
vlad ageysha <ander123fakk@gmail.com>
2021-01-21 09:26:16 +0300

    fixed switching between tabs

--- src/App/App.js
+++ src/App/App.js
@@ -15,26 +15,15 @@ const App = () => {
 
         localStorage.setItem('notes', JSON.stringify(notes));
     }
-
-    function switchWindow(e) {
-        const btn = e.target;
-        const tabList = btn.parentElement.parentElement;
-
-        for (let i = 0; i < tabList.children.length; i++) {
-            tabList.children[i].firstChild.classList.remove('active-tab');
-        }
-
-        btn.classList.add('active-tab');
-    }
-
+    
     return(
         <div className="app">
             <HashRouter>
 
                 <ul className="tab-list">
-                    <NavLink to="/create"><li className="tab" onClick={(e) => switchWindow(e)}>Создать</li></NavLink>
-                    <NavLink to="/"><li className="tab active-tab" onClick={(e) => switchWindow(e)}>Заметки</li></NavLink>
-                    <NavLink to="/done-notes"><li className="tab" onClick={(e) => switchWindow(e)}>Готовые</li></NavLink>
+                    <NavLink to="/create"><li className="tab">Создать</li></NavLink>
+                    <NavLink exact to="/"><li className="tab">Заметки</li></NavLink>
+                    <NavLink to="/done-notes"><li className="tab">Готовые</li></NavLink>
                 </ul>
                 <div className="content">
                     <Route path="/create" render={props => <Form updateData={updateData} />} />
